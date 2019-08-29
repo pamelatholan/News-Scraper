77635e00c5f3bcd72233b5abb2f18417a24570f2
Pam Tholan <pamelatholan@yahoo.com>
2019-08-28 22:41:19 -0400

    fixed axios issue to scrape articles

--- models/article.js
+++ models/article.js
@@ -2,33 +2,33 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var Articleschema = new Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	link: {
-		type: String,
-		required: true,
-	},
-	issaved: {
-		type: Boolean,
-		default: false
-	},
-	status: {
-		type: String,
-		default: "Save Article"
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	},
-	note: {
-		type: Schema.Types.ObjectId,
-		ref: "Note"
-	}
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  issaved: {
+    type: Boolean,
+    default: false
+  },
+  status: {
+    type: String,
+    default: "Save Article"
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
-Articleschema.index({title: "text"});
+Articleschema.index({ title: "text" });
 
-var Article  = mongoose.model("Article", Articleschema);
+var Article = mongoose.model("Article", Articleschema);
 module.exports = Article;
--- models/note.js
+++ models/note.js
@@ -2,12 +2,12 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var Noteschema = new Schema({
-	title: {
-		type: String,
-	},
-	body: {
-		type: String,
-	}
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
 });
 
 var Note = mongoose.model("Note", Noteschema);
